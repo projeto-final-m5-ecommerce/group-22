3b1c47fa741bcb9ab9d8ae69806e490a3e220b48
lucana-m <lucana_monica@hotmail.com>
2023-05-04 16:05:50 -0300

    fix: cart model

--- orders/views.py
+++ orders/views.py
@@ -2,21 +2,23 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
-from ..carts.models import Cart
-from ..users.models import User
+import ipdb
+from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.permissions import IsAuthenticatedOrReadOnly
 
 
 class OrderView(generics.CreateAPIView):
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticatedOrReadOnly]
+
     queryset = Order.objects.all()
     serializer_class = OrderSerializer
 
     def perform_create(self, serializer):
-        # cart_id = self.kwargs.get("pk")
-        # cart = Cart.objects.filter(id=cart_id).values()
-        # list_products = cart.
-
-        # user = self.request.user.
+        # users = User.objects.all()[0]
+        # user = self.request.user
+        # ipdb.set_trace()
         # list_products = user.
 
         # serializer.save()
-        return serializer.save(user_id=self.kwargs.get("pk"))
+        return serializer.save()
