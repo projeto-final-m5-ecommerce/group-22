fb407d2c2e20f8c06436bc52b4884ce7e84063b6
Fernanda Bollinger <feernanda.bollinger@gmail.com>
2023-05-04 14:22:06 -0300

    feat: products views and fix product serializer

--- users/urls.py
+++ users/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
 from . import views
+from products import views as product_views
+
 
 from rest_framework_simplejwt import views as jwt_views
 
@@ -8,4 +10,5 @@ urlpatterns = [
     path("users/<int:pk>/", views.UserDetailView.as_view()),
     path("users/<int:pk>/update", views.UserUpdateTypeView.as_view()),
     path("users/login/", jwt_views.TokenObtainPairView.as_view()),
+    path("users/<int:pk>/product/", product_views.ProductView.as_view()),
 ]
