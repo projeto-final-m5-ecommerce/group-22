ab709da050b9070e55f809fc81a4f0bb74f10a80
lucana-m <lucana_monica@hotmail.com>
2023-05-08 10:39:06 -0300

    fix: orders

--- orders/views.py
+++ orders/views.py
@@ -2,9 +2,10 @@ from django.shortcuts import render
 from rest_framework import generics
 from .models import Order
 from .serializers import OrderSerializer
-import ipdb
 from rest_framework_simplejwt.authentication import JWTAuthentication
 from rest_framework.permissions import IsAuthenticatedOrReadOnly
+
+# import ipdb
 
 
 class OrderView(generics.CreateAPIView):
@@ -15,10 +16,4 @@ class OrderView(generics.CreateAPIView):
     serializer_class = OrderSerializer
 
     def perform_create(self, serializer):
-        # users = User.objects.all()[0]
-        user = self.request.user.cart
-        ipdb.set_trace()
-        # list_products = user.
-
-        # serializer.save()
-        return serializer.save(user_id=self.request.user.id)
+        return serializer.save(user=self.request.user)
