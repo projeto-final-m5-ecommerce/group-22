b3bf894cd7bce4ee66263f67c922216dacd65b49
lucana-m <lucana_monica@hotmail.com>
2023-05-08 22:51:35 -0300

    fix: order serializer

--- orders/serializers.py
+++ orders/serializers.py
@@ -1,6 +1,8 @@
 from rest_framework import serializers
 from .models import Order
 from carts.models import Cart, ProductsCart
+from django.core.mail import send_mail
+from django.conf import settings
 
 import ipdb
 
@@ -31,29 +33,30 @@ class OrderSerializer(serializers.ModelSerializer):
 
         return list_products
 
-    # def get_total(self, obj):
-    #     id_user = obj.user.id
-    #     filtered_cart = Cart.objects.filter(user=id_user)
-    #     user_cart = filtered_cart[0].cart_products.all()
-    #     all_values = [product.price for product in user_cart]
-    #     return sum(all_values)
-
     def get_total(self, obj):
         id_user = obj.user.id
         filtered_cart = Cart.objects.filter(user=id_user)
-        cart_id = filtered_cart[0].id
-        ipdb.set_trace()
-        current_cart = ProductsCart.objects.filter(cart=cart_id)
-        # all_values = [product.price for product in user_cart]
-        # return "100"
+        user_cart = filtered_cart[0].cart_products.all()
+        all_values = [product.price for product in user_cart]
+        return sum(all_values)
 
     def create(self, validated_data):
         return Order.objects.create(**validated_data)
 
     def update(self, instance, validated_data):
+        user_email = self.user.email
+
         for key, value in validated_data.items():
             # so vendedor pode alterar status
-            setattr(instance, key, value)
+            if key == "status" and value != "Order placed":
+                send_mail(
+                    subject="Order Status",
+                    message="Your order status has been updated.",
+                    from_email=settings.EMAIL_HOST_USER,
+                    recipient_list=[user_email],
+                    fail_silently=False,
+                )
+                setattr(instance, key, value)
 
         instance.save()
 
