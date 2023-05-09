ba562935a268b93ab269d2364d8a61359bfe4b44
lucana-m <lucana_monica@hotmail.com>
2023-05-09 17:48:57 -0300

    fix: finished cart

--- products/models.py
+++ products/models.py
@@ -7,6 +7,7 @@ class Product(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     stock = models.IntegerField(default=0)
     available = models.BooleanField(default=True)
+    quantity = models.PositiveIntegerField(default=0)
 
     user = models.ForeignKey(
         "users.User", on_delete=models.CASCADE, related_name="products"
